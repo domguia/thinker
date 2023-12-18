287c42695dd3d5556bce1d67e601f0b0f41024a7
domguia <123229723+domguia@users.noreply.github.com>
2023-12-18 21:40:59 +0000

    refractor data

--- numbers_data.py
+++ numbers_data.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from thinker_model import CfgNode
 # MAX_NUMBER = 1e10
 
 MAX_BASE = 16 + 1 #256
@@ -41,7 +43,7 @@ def to_base(n, base):
 # def change_base_generator(batch=2, max_number=1000):
 #     return np.random.randn(batch, max_number)
 
-def add_mult_generator(batch=2, step=2, max_number=1000, operations='+-*', operation_dist=None, **kwargs):
+def add_mult_generator(batch=2, step=2, max_number=1000, operations='+-*', operation_dist=None, task_id=None, **kwargs):
     _step = sum(step)/2 if step is set else step
     numbers = np.random.randint(max_number, size=(batch, _step))
     applied_op = np.random.choice(list(operations), size=(batch,  _step), p=operation_dist)
@@ -188,83 +190,47 @@ TASK_SCHEME = {
     }
 }
 
-# def generate(...):
-
-#     # TODO: should samples task base on task_scheme definition
-#     samples = factorize_generator(batch=2, step=6, n_first_prime=10)
-#     samples = add_mult_generator(batch=2, step=4, max_number=1000, operations='+-*/', operation_dist=[0.1,0.1,0.4,0.4])
-#     task_id = ':addmul'
-
-#     # TODO: should samples in_base and out_base looking at task_scheme definition
-#     in_base = 10
-#     out_base = 10
-
-#     all_x = []
-#     all_y = []
-#     for x,y in zip(*samples):
-#         x = apply_to_base(x, in_base)
-#         y = apply_to_base(y, out_base)
-        
-#         # add input trriger
-#         x = [in_base, '@'] + x + [task_id, '@=', out_base]
-
-#         # tokenize
-#         x = token_encoder(x)
-#         y = token_encoder(y)
-
-#         # save
-#         all_x.append(x)
-#         all_y.append(y)
-
-#     # get max lenght for padding
-#     x_max_len = max([len(x) for x in all_x])
-#     y_max_len = max([len(y) for y in all_y])
-
-#     # apply padding
-#     blank_token = token_encoder('blank')
-#     all_x = [     [blank_token]*(x_max_len-len(x)) + x for x in all_x] # pad begin
-#     all_y = [ y + [blank_token]*(y_max_len-len(y))     for y in all_y] # pad end
-
-#     all_x = np.stack(all_x, dtype=np.int16)
-#     all_y = np.stack(all_y, dtype=np.int16)
-
 
 import numpy as np
 import torch
 from torch.utils.data import IterableDataset, DataLoader
 
-def sample_base(values, distribution=None):
+def sample_base(values, batch=None, distribution=None):
     # Normalize the distribution to sum to 1
     # distribution = np.array(distribution)
     # distribution = distribution / distribution.sum()
     # Sample a base according to the probability distribution
-    base = np.random.choice(values, p=distribution)
+    base = np.random.choice(values, size=batch, p=distribution)
     return base
 
 # Update the task_scheme definition with an actual distribution
 # task_scheme['input']['base']['distribution'] = [1 / (2 ** i) for i, _ in enumerate(task_scheme['input']['base']['values'])]
 
 class NumbersCopyDataset(IterableDataset):
-    def __init__(self, low, high, size, flip=False, roll=False):
+    def __init__(self, low, high, size, flip=False, roll=False, sort=False):
         self.low = low
         self.high = high
         self.size = size
         self.flip = flip
         self.roll = roll
+        self.sort = sort
 
     def __iter__(self):
         while True:
             x = torch.randint(self.low, self.high, self.size)
-            y = x
+            y = x #.clone()
+            if self.sort:
+                y = x.sort()[0]
             if self.flip:
                 y = x.flip(dims=(0,))
-            if self.roll:    
+            if self.roll:
                 y = x.roll(x[0].item(), 0)
             yield x, y
 
 class NumbersComputeDataset(IterableDataset):
-    def __init__(self, task_scheme):
-        self.task_scheme = task_scheme
+    def __init__(self, cfg):
+        # self.task_scheme = task_scheme
+        self.cfg = cfg
     
     @staticmethod
     def get_vocabulary():
@@ -275,72 +241,70 @@ class NumbersComputeDataset(IterableDataset):
         return len(VOCABULARY)
 
     def __iter__(self):
-        # task_keys = list(self.task_scheme['tasks'].keys())
-        task_keys = ['add_mult_generator', 'factorize_generator']
-        task_probabilities = self.task_scheme['tasks']['shared_args']['task_probabilities']
 
         while True:
-            # Sample task according to the probabilities
-            task_key = np.random.choice(task_keys, p=task_probabilities)
-            task_config = self.task_scheme['tasks'][task_key]
+            # TODO: should samples task base on task_scheme definition
+            # samples = factorize_generator(batch=2, step=6, n_first_prime=10)
+            # samples = add_mult_generator(batch=2, step=4, max_number=1000, operations='+-*/', operation_dist=[0.1,0.1,0.4,0.4])
+            samples = add_mult_generator(**self.cfg.__dict__)
+            task_id = ':addmul'
 
+            # TODO: should samples in_base and out_base looking at task_scheme definition
             # Sample input and output bases
-            in_base = sample_base(**self.task_scheme['input']['base'])
-            out_base = sample_base(**self.task_scheme['output']['base'])
-            # Here we assume output base is same as input base; replace with another sampling if different
+            in_bases = sample_base(self.cfg.in_bases, self.cfg.batch, self.cfg.in_bases_dist)
+            out_bases = sample_base(self.cfg.out_bases, self.cfg.batch, self.cfg.out_bases_dist)
 
-            # Prepare parameters for the generator function
-            gen_params = {**task_config, **self.task_scheme['tasks']['shared_args']}
-            gen_params.update(step=np.random.randint(*task_config['step']))
+            all_x = []
+            all_y = []
+            for x,y, in_base,out_base in zip(*samples, in_bases, out_bases):
+                x = apply_to_base(x, in_base)
+                y = apply_to_base(y, out_base)
+                
+                # add input trriger
+                x = [in_base, '@'] + x + [task_id, '@=', out_base]
 
-            # Invoke the chosen task's generator function with prepared parameters
-            if task_key == 'factorize_generator':
-                x, y = factorize_generator(**gen_params)
-            elif task_key == 'add_mult_generator':
-                x, y = add_mult_generator(**gen_params)
+                # tokenize
+                x = token_encoder(x)
+                y = token_encoder(y)
 
-            # Apply base conversion, tokenization, and padding
-            for single_x, single_y in zip(x, y):
-                single_x = single_y # HACK: should be done differently
-                single_x = apply_to_base(single_x, in_base)
-                single_y = apply_to_base(single_y, out_base)
+                # save
+                all_x.append(x)
+                all_y.append(y)
 
-                # Add task marker to the input
-                single_x = [in_base, '@'] + single_x + [task_config['task_id'], '@=', out_base]
+            # get max lenght for padding
+            x_max_len = max([len(x) for x in all_x])
+            y_max_len = max([len(y) for y in all_y])
 
-                # Tokenization
-                tokenized_x = token_encoder(single_x)
-                tokenized_y = token_encoder(single_y)
+            # apply padding
+            blank_token = token_encoder('blank')
+            all_x = [     [blank_token]*(x_max_len-len(x)) + x for x in all_x] # pad begin
+            all_y = [ y + [blank_token]*(y_max_len-len(y))     for y in all_y] # pad end
 
-                # Padding
-                input_max_length = self.task_scheme['input']['max_length']
-                output_max_length = self.task_scheme['output']['max_length']
-                padded_x = self.pad_sequence(tokenized_x, input_max_length, VOCABULARY_DICT['blank'])
-                padded_y = self.pad_sequence(tokenized_y, output_max_length, VOCABULARY_DICT['blank'], back=True)
+            all_x = np.stack(all_x)#, dtype=np.int16)
+            all_y = np.stack(all_y)#, dtype=np.int16)
+                
+            yield torch.IntTensor(all_x), torch.IntTensor(all_y)
 
-                if len(padded_x)>input_max_length or len(padded_y)>output_max_length:
-                    continue # skip the sample if overflow
-
-                # Return the tokenized and padded sequences, dtype=np.int16 fail at embedding lookup
-                yield np.array(padded_x, dtype=np.int64), np.array(padded_y, dtype=np.int64)
-
-    @staticmethod
-    def pad_sequence(sequence, max_length, pad_value, back=False):
-        # Pads sequence to the max length with the given pad_value
-        if back:
-            return sequence + [pad_value] * (max_length - len(sequence))
-        else:
-            return [pad_value] * (max_length - len(sequence)) + sequence
     
 
 if __name__ == '__main__':
-    dataset = NumbersComputeDataset(TASK_SCHEME)
-    # batch_size = task_scheme['tasks']['shared_args']['batch']  # Set the batch size based on the scheme
-    batch_size = 32
-    dataloader = DataLoader(dataset, batch_size=batch_size)
 
-    # Example of iterating through the data loader (add stopping criteria as needed)
-    for batch_idx, (x, y) in enumerate(dataloader):
-        print(f"Batch {batch_idx}: x={x.shape}, y={y.shape}")
-        if batch_idx >= 1:  # Stop after 2 batches for this example
-            break
+    cfg = CfgNode(batch=5, step=4, max_number=1000, operations='+-*/', operation_dist=[0.1,0.1,0.4,0.4],
+               in_bases=[2,4,8,16], in_bases_dist=None,
+               out_bases=[2,4,8,16], out_bases_dist=[.1,.2,.3,.4])
+    dataset = NumbersComputeDataset(cfg)
+
+    x, y = next(iter(dataset))
+    print(f"Batch : x={x.shape}, y={y.shape}")
+    
+    # dataset = NumbersComputeDataset(TASK_SCHEME)
+
+    # batch_size = task_scheme['tasks']['shared_args']['batch']  # Set the batch size based on the scheme
+    # batch_size = 32
+    # dataloader = DataLoader(dataset, batch_size=batch_size)
+
+    # # Example of iterating through the data loader (add stopping criteria as needed)
+    # for batch_idx, (x, y) in enumerate(dataloader):
+    #     print(f"Batch {batch_idx}: x={x.shape}, y={y.shape}")
+    #     if batch_idx >= 1:  # Stop after 2 batches for this example
+    #         break
