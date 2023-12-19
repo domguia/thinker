bea6aa07a063f5278ec807493c4ad1db3079653b
domguia <123229723+domguia@users.noreply.github.com>
2023-12-19 00:02:18 +0000

    draft trainer

--- th1nker_run_lightning.py
+++ th1nker_run_lightning.py
@@ -5,94 +5,102 @@ import lightning as L
 from torch.utils.data import DataLoader
 
 from thinker_model import Th1nker, compute_loss, CfgNode
-from numbers_data import NumbersComputeDataset, TASK_SCHEME
+from numbers_data import NumbersComputeDataset
 
-cfg = CfgNode(
-    hdim = 32,
-    head_size = 4,
-    number_of_head= 8,
-    resid_pdrop = 0.1,
-    attn_pdrop = 0,
-    bias=False,
+model_cfg = CfgNode(
+    # hdim = 32,
+    # head_size = 4,
+    # number_of_head= 8,
+    # resid_pdrop = 0.1,
+    # attn_pdrop = 0.1,
+    # bias=False,
 
-    vocab_size = 270,
-    
-    input_cache_size = 64,
-    mem_cache_size = 512,
+    # vocab_size = 31+1,
 
-    min_latent_size = 8,
-    max_latent_size = 32,
-    max_output_len = 32,
-    
-    min_step=2,
-    max_step=7,
+    # input_cache_size = 24,
+    # mem_cache_size = 128,
+    # max_output_len = 24,
 
-    probe_mode="number_reg",
-    good_pred_loss_treshold=0.5,
-    decay_coef=4,
+    vocab_size = 31+1,
+    max_latent=64,
+    max_input_len=48,
+    output_len=40,
+    d_model=32,
+    nhead=8,
+    d_hid=32*4,
+    nlayers=1,
+    dropout=0
+)
+
+data_cfg = CfgNode(batch=2, step=2, max_number=1_000, operations='+-*/', operation_dist=[0,0,1.0,0],
+    in_bases=[16], in_bases_dist=None, # [2,4,8,16] # list(range(2,8+1))
+    out_bases=[16], out_bases_dist=None, # [.1,.2,.3,.4])
+)
+
+run_cfg = CfgNode(
+    n_latent = 8, # range(4,8)
+    n_step=4, # range(1,4)
+
+    max_iter=5000,
+    learning_rate=0.005,
+    batch = 1*1024,
+)
+
+exp_cfg = CfgNode(
+    n_latent = range(4,8+1),
+    n_step = range(1,4+1),
 )
 
 # define the LightningModule
 class LitTh1nker(L.LightningModule):
-    def __init__(self, config):
-        super().__init__()
-        self.model = Th1nker(config)
-        self.cfg = config
+    def __init__(self, ):
+        super().__init__(model_cfg, run_cfg)
+        self.model = ToyThinker(**model_cfg.__dict__)
+        self.cfg = run_cfg
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
         inputs, targets = batch
+        inputs, targets = inputs[0], targets[0] # implicit batch trick
 
         B, T = inputs.size()
 
-        n_step = np.random.randint(self.cfg.min_step, self.cfg.max_step)
-        latent_size = np.random.randint(self.cfg.min_latent_size, self.cfg.max_latent_size+1)
+        n_step = self.cfg.sample('n_step')
+        n_latent = self.cfg.sample('n_latent')
 
-        # with torch.device(device):
-        self.model.init(batch_size, latent_size)
-        self.model.load_input(inputs)
+        n_target = targets.size(1)
+        logits, output_probe = model(inputs, n_latent, n_target, n_step)
 
-        for i in range(n_step-1):
-            # with torch.device(device):
-            self.model.compute_step()
+        # compute loss
+        output_loss = torch.nn.functional.cross_entropy(logits.permute(0,2,1), targets.long()) #, ignore_index=20)
+        probe_loss = torch.nn.functional.mse_loss(output_probe.squeeze(-1), targets.float()/3)
 
-        # with torch.device(device):
-        self.model.compute_step(with_output=targets.size(1))
-        output = self.model.get_output()
-        
-        loss = compute_loss(output, targets, cfg.probe_mode)
-        _, probe_loss, pred_loss, output_losses, outputs_probe_losses = loss
-        
-        output_loss = output_losses.mean()
-        outputs_probe_loss = outputs_probe_losses.mean()
-        
-        # loss fusion
-        loss = (output_loss + outputs_probe_loss*16*4)
+        loss = output_loss + probe_loss
 
         # Logging to TensorBoard (if installed) by default
         # self.log("step", B*batch_idx) # for weight and bias
         self.log("sample", B*batch_idx*1.0)
         # self.log("batch_idx", batch_idx)
         self.log("output_loss", output_loss)
-        self.log("outputs_probe_loss", outputs_probe_loss)
+        self.log("outputs_probe_loss", probe_loss)
         self.log("train_loss", loss)
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.005)
         return optimizer
 
 
 # init the autoencoder
-model = LitTh1nker(cfg)
-
+model = LitTh1nker(model_cfg, run_cfg)
 
 # init data loader
-dataset = NumbersComputeDataset(TASK_SCHEME)
-batch_size = 27
-dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=2)
+data_cfg.batch = run_cfg.batch # implicit batch trick
+batch_size = 1
 
+dataset = NumbersComputeDataset(data_cfg)
+dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=4)
 
 # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
 trainer = L.Trainer(limit_train_batches=100, max_epochs=1)
