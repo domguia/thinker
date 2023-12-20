004504bc98d5c09d0ea2d37566120354787ecdc9
joseph <beconnect746@gmail.com>
2023-12-20 19:27:47 +0200

    add __repr__() and loop_over_extremum(...)

--- utils.py
+++ utils.py
@@ -26,6 +26,8 @@ class MatchCount:
         for c in mean[:self.max]:
             out += f" {c:.2f} "
         return out
+    def __repr__(self):
+        return self.__dict__.__str__()
 
 ### test
 # match_count = MatchCount(20)
@@ -44,6 +46,9 @@ class CfgNode:
         self.__dict__.update(**{name: globals()[name] for name in args})
     def __str__(self):
         return self.__dict__.__str__()
+    def __repr__(self):
+        return self.__dict__.__repr__()
+
     
     # sample variable parameters
     def sample(self, param):
@@ -63,6 +68,7 @@ class CfgNode:
 import pandas as pd
 import seaborn as sns
 
+# training visualization utils
 def plot_loss_and_accuracy(logs, experiment=None):
     # Create a DataFrame from the list of dictionaries.
     if isinstance(logs, pd.DataFrame): df = logs
@@ -75,9 +81,10 @@ def plot_loss_and_accuracy(logs, experiment=None):
 
     # Plot the loss values on the left y-axis.
     sns.lineplot(data=df, x=df.index, y='loss', color='orange') #, hue='step')
-    sns.lineplot(data=df, x=df.index, y='probe_loss', color='peru') #, hue='step')
+    if 'probe_loss' in df.columns:
+        sns.lineplot(data=df, x=df.index, y='probe_loss', color='peru') #, hue='step')
     plt.ylabel('Loss')
-    plt.yscale('log',base=2)
+    # plt.yscale('log',base=2)
 
     # Plot the accuracy values on the right y-axis.
     plt.twinx()
@@ -97,6 +104,7 @@ def plot_loss_and_accuracy(logs, experiment=None):
 
     #plot_hp_heatmap(logs)
 
+# training visualization utils
 def plot_hp_heatmap(logs, use_last_n_batch=200, aggr='mean'):
     if isinstance(logs, pd.DataFrame): df = logs
     else: df = pd.DataFrame(logs)
@@ -122,3 +130,29 @@ def plot_hp_heatmap(logs, use_last_n_batch=200, aggr='mean'):
 
     # Show the plot.
     plt.show()
+
+# training utils
+def loop_over_extremum(*args, corner=True, edge=True):
+    # product('ABCD', 'xyz') --> Ax Bx Cx Dx   Az Bz Cz Dz
+
+    # n = len(args)
+    # for i in range(n):
+    #     min = args[i][0]
+    #     max = args[i][-1]
+    # for ext in [min,max]:
+    #     for j in range(n):
+    #         if j != i:
+    #             for val in args[j]:
+    #                 yield (ext, val)
+    n = len(args)
+    args_ = [tuple(pool) for pool in args]
+    maxs_ = [len(pool)-1 for pool in args]
+    pools = [tuple(range(len(pool))) for pool in args]
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        n_extremum = sum([ (p==0 or p==m) for m,p in zip(maxs_,prod)])
+        if (corner and n==n_extremum) or (edge and (n-1)==n_extremum) :
+            yield tuple([arg[p] for arg,p in zip(args,prod)])
+
