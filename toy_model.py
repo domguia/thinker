8e42e7c88025af21cfec02de57db34d6d0ac1762
domguia <123229723+domguia@users.noreply.github.com>
2023-12-20 04:13:02 +0000

    refactor + lite decoder + improve compute

--- toy_model.py
+++ toy_model.py
@@ -7,39 +7,46 @@ from torch.nn import TransformerDecoder, TransformerDecoderLayer
 class ToyThinker(nn.Module):
 
     def __init__(self, vocab_size: int, max_latent: int, max_input_len: int, output_len: int, d_model: int, nhead: int, d_hid: int,
-                 nlayers: int, dropout: float = 0.1):
+                 nlayers: int, n_probe: int = 0, dropout: float = 0.1):
         super().__init__()
         # self.pos_encoder = PositionalEncoding(d_model, dropout)
-        decoder_layers = TransformerDecoderLayer(d_model, nhead, d_hid, dropout, batch_first=True)
+        decoder_layers = TransformerDecoderLayer(d_model, nhead, d_hid, dropout, activation=F.gelu, batch_first=True)
         self.compute_step = TransformerDecoder(decoder_layers, nlayers)
 
-        decoder_layers = TransformerDecoderLayer(d_model, nhead, d_model, dropout, batch_first=True) # n_hid = d_model*2 because shoul be smaller
+        decoder_layers = LiteTransformerDecoderLayer(d_model, nhead, d_model, dropout, activation=F.gelu, batch_first=True) # n_hid = d_model*2 because shoul be smaller
         self.compute_output = TransformerDecoder(decoder_layers, 1) # only one layer
 
         self.embedding = nn.Embedding(vocab_size, d_model)
         self.latent_embedding = nn.Embedding(max_latent, d_model)
         self.pos_embedding_in  = nn.Embedding(max_input_len, d_model)
         self.pos_embedding_out = nn.Embedding(output_len, d_model)
-        self.d_model = d_model
-        self.linear = nn.Linear(d_model, vocab_size)
-        self.linear_probe = nn.Linear(d_model, 1)
+        
+        self.linear = nn.Linear(d_model, vocab_size + n_probe)
 
+        self.d_model = d_model
         self.vocab_size = vocab_size
         self.max_input_len = max_input_len
         self.output_len = output_len
+        self.n_probe = n_probe
 
         self.init_weights()
 
     def init_weights(self) -> None:
         initrange = 0.1
-        self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.latent_embedding.weight.data.uniform_(-initrange, initrange)
-        self.pos_embedding_in.weight.data.uniform_(-initrange, initrange)
-        self.pos_embedding_out.weight.data.uniform_(-initrange, initrange)
-        self.linear.bias.data.zero_()
-        self.linear.weight.data.uniform_(-initrange, initrange)
+        def _apply_init_weights(module):
+            module.weight.data.uniform_(-initrange, initrange)
+            if isinstance(module, nn.Linear):
+                module.bias.data.zero_()
 
-    def forward(self, x: Tensor, n_latent: int = None, n_target: int = None, n_step: int = 1) -> Tensor:
+        [_apply_init_weights(module) for module in (
+            self.embedding,
+            self.latent_embedding,
+            self.pos_embedding_in,
+            self.pos_embedding_out,
+            self.linear,
+        )]
+
+    def forward(self, x: Tensor, n_latent: int = None, n_target: int = None, n_step: int = 1, read_step: int = 1e4, step_output:int = 1, n_memory:int = -1) -> Tensor:
         """
         Arguments:
             x: Tensor, shape ``[seq_len, batch_size]``
@@ -58,21 +65,36 @@ class ToyThinker(nn.Module):
         if n_target == None: n_target = T
         if n_latent == None: n_latent = 8
 
-        out_query = self.pos_embedding_out(pos[:,:n_target])
         latent = self.latent_embedding(pos[:,:n_latent])
+        out_query = self.pos_embedding_out(pos[:,:n_target])
 
+        latents = []
+        outputs = []
         memory = x
-        latent = out_query
         for i in range(n_step):
-            # self.transformer_decoder.forward(tgt, memory, ...
             latent = self.compute_step(latent, memory)
-            memory = torch.cat((memory,latent), dim=1)
 
-        output_emb = self.compute_output(out_query, memory)
-        output = self.linear(output_emb)
-        output_probe = self.linear_probe(output_emb)
+            # append to latents memory
+            latents.append(latent)
+            if len(latents) > n_memory: latents.pop(0) # first in first out
 
-        return output, output_probe
+            # compute context : memory + input
+            memory = latents if i>read_step else latents + [x] 
+            memory = torch.cat(memory, dim=1)
+
+            if i >= (n_step - step_output):
+                # compute output at the last step
+                output = self.compute_output(out_query, memory) # B, T, H
+                outputs.append(output)
+
+        outputs = torch.cat(outputs, dim=1) # B, S, T, H
+        outputs = self.linear(outputs)
+
+        # split outputs
+        logits = outputs[:, :, :self.vocab_size] # B, S, T, vocab_size
+        probes = outputs[:, :, -self.n_probe:]   # B, S, T, n_probe
+
+        return logits, probes
     
 class PositionalEncoding(nn.Module):
 
@@ -96,10 +118,45 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
     
 
+class LiteTransformerDecoderLayer(TransformerDecoderLayer):
+    def __init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
+        # we dont need self attention
+        self.self_attn = None
+        self.norm1 = None
+        self.dropout1 = None
+    
+    def forward(
+        self,
+        tgt: Tensor,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = None,
+        memory_mask: Optional[Tensor] = None,
+        tgt_key_padding_mask: Optional[Tensor] = None,
+        memory_key_padding_mask: Optional[Tensor] = None,
+        tgt_is_causal: bool = False,
+        memory_is_causal: bool = False,
+    ) -> Tensor:
+        
+        # https://pytorch.org/docs/stable/_modules/torch/nn/modules/transformer.html#TransformerDecoderLayer.forward
+        x = tgt
+        if self.norm_first:
+            # disable self attention block
+            # x = x + self._sa_block(self.norm1(x), tgt_mask, tgt_key_padding_mask, tgt_is_causal)
+            x = x + self._mha_block(self.norm2(x), memory, memory_mask, memory_key_padding_mask, memory_is_causal)
+            x = x + self._ff_block(self.norm3(x))
+        else: # default case we runing in this project
+            # disable self attention block
+            # x = self.norm1(x + self._sa_block(x, tgt_mask, tgt_key_padding_mask, tgt_is_causal))
+            x = self.norm2(x + self._mha_block(x, memory, memory_mask, memory_key_padding_mask, memory_is_causal))
+            x = self.norm3(x + self._ff_block(x))
+
+        return x
+
 if __name__ == '__main__':
     model = TransformerModel(vocab_size=17, d_model=32, output_len=5, nhead=4, d_hid=8,
                  nlayers=1, dropout=0.1)
     x = torch.randint(0,16, (2,5))
-    y = model(x)
+    y = model(x, n_latent = 3, n_target = 5, n_step = 4, read_step = 2)
     print("y.shape:", y.shape)
 
